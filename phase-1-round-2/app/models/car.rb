d6d3c789c264c45346ef298acdb870bd831544fe
Raul Sanchez <raulsg93@gmail.com>
2021-03-26 10:02:15 -0500

    added phase-2

--- phase-1-round-2/app/models/car.rb
+++
@@ -1,7 +0,0 @@
-class Car
-  attr_reader :passenger_count
-
-  def initialize(passenger_count)
-    @passenger_count = passenger_count
-  end
-end
--- phase-1-round-2/app/models/movie_screen.rb
+++
@@ -1,9 +0,0 @@
-class MovieScreen
-  attr_reader :capacity, :movie_title, :drive_in  
-
-  def initialize(movie_title, capacity, drive_in)
-    @movie_title = movie_title
-    @capacity = capacity
-    @drive_in = drive_in
-  end
-end
